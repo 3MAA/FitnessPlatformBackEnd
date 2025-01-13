aec702159887ccc063a682fe630352d4b6fc5dd1
Ema <emanuela.lupancu@student.usv.ro>
2025-01-13 21:40:59 +0200

    Creating an API for registration

---
+++ FitnessPlatform/Models/DTOs/RegisterModelDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessPlatform.Models.DTOs
+{
+    public class RegisterModelDto
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string? UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string? UserPassword { get; set; }
+    }
+}
